54206657c73147e07f9de96eeb0af929c70ea46c
Alberto Valenzuela <avalen28@gmail.com>
2023-06-25 20:19:47 +0200

    Adds logic to make sure that status of tasks is updated based on a unique property, the id of the task

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 interface Task {
+  id: number;
   name: string;
   isFinished: boolean;
 }
@@ -21,21 +22,34 @@ export class TodolistComponent {
   }
 
   addToList(): void {
+    const lastItemFromList = this.taskList[this.taskList.length - 1];
+    
+    let idForNewTask;
+
+    if (this.taskList.length === 0) {
+      idForNewTask = 1;
+    } else {
+      idForNewTask = lastItemFromList.id + 1;
+    }
+
     this.taskList.push({
+      id: idForNewTask,
       name: this.newTask,
       isFinished: false
     });
+
     this.newTask = "";
   }
 
-  changeTaskStatus(nameOfTaskToUpdate: string): void {
+  changeTaskStatus(idOfTaskToUpdate: number): void {
     const updatedTaskList = this.taskList.map(task => {
-      if (task.name !== nameOfTaskToUpdate) {
+      if (task.id !== idOfTaskToUpdate) {
         return task
       }
       task.isFinished = !task.isFinished;
       return task
     });
+
     this.taskList = updatedTaskList;
   }
 }
